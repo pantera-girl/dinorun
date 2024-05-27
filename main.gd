7089cb35cb0b14a36a0ca18d64ba56074a24cd78
susana <susana.cubillan@gmail.com>
2024-05-27 11:00:13 -0600

    me faltaba el mensaje

--- main.gd
+++ main.gd
@@ -2,13 +2,15 @@ extends Node2D
 
 var screen_size : Vector2i
 var PlayerInitialPosition
-
 func _ready():
 	screen_size = get_window().size
 	PlayerInitialPosition = $player.position
 
 
-
+func new_game():
+	$Floor.position.x = 0
+	$player.position = PlayerInitialPosition
+	
 func _process(delta):
 	$Floor.position.x = $player.position.x -150
 	
